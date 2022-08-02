3cf492e4c9f14e5c34db0a015ce20298c29ea838
Ghevond Tunyan <ghtunyan2018@gmail.com>
2022-08-02 19:49:45 +0200

    React Router Setup

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import About from './AboutPage'
+import Home from './HomePage'
+import Error from './ErrorPage'
+import Checkout from './CheckoutPage'
+import Cart from './CartPage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import PrivateRoute from './PrivateRoute'
+
+export {
+  About,
+  Home,
+  Error,
+  Checkout,
+  Cart,
+  Products,
+  SingleProduct,
+  PrivateRoute,
+}
