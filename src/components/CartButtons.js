b6d9801183555ba7b013a888fcfd2dd68c3689b7
Ghevond Tunyan <ghtunyan2018@gmail.com>
2022-08-04 19:04:54 +0200

    add sidebar and its functionality

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -8,7 +8,7 @@ import { useUserContext } from '../context/user_context'
 
 const CartButtons = () => {
   return (
-    <Wrapper>
+    <Wrapper className='cart-btn-wrapper'>
       <Link to='/cart' className='cart-btn'>
         Cart
         <span className='cart-container'>
