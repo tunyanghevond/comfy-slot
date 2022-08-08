94f3dfe4350db41d0b727201110d5c716b19950f
Ghevond Tunyan <ghtunyan2018@gmail.com>
2022-08-08 19:20:15 +0200

    add home page content

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,31 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <h3>Join our newsletter and 20% off </h3>
+        <div className='content'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, a
+            aliquam cupiditate, vel ratione deserunt nihil corporis dicta fugit
+            perferendis earum assumenda. Repellat, ullam commodi. Cumque sequi
+            earum quos! Vel.
+          </p>
+          <form className='contact-form'>
+            <input
+              type='email'
+              placeholder='enter email'
+              className='form-input'
+            />
+            <button type='submit' className='submit-btn'>
+              subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
