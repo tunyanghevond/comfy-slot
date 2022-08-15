3b576ecccce8972009b4f07cf0f4c3eec84b3c78
Ghevond Tunyan <ghtunyan2018@gmail.com>
2022-08-15 19:31:11 +0200

    add: stars

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -56,6 +56,7 @@ const SingleProductPage = () => {
     company,
     images,
   } = product
+  console.log(product)
 
   return (
     <Wrapper>
@@ -68,7 +69,7 @@ const SingleProductPage = () => {
           <ProductImages images={images} />
           <section className='content'>
             <h2>{name}</h2>
-            <Stars />
+            <Stars stars={stars} reviews={reviews} />
             <h5 className='price'>{formatPrice(price)}</h5>
             <p className='desc'>{description}</p>
             <p className='info'>
