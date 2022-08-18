541424208c817e23f548987fc965b57c1b9b9b9d
Ghevond Tunyan <ghtunyan2018@gmail.com>
2022-08-18 17:34:14 +0200

    products -page: setup grid view and List view

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -15,6 +15,7 @@ import { useProductsContext } from './products_context'
 const initialState = {
   filtered_products: [],
   all_products: [],
+  grid_view: true,
 }
 
 const FilterContext = React.createContext()
@@ -27,7 +28,7 @@ export const FilterProvider = ({ children }) => {
     dispatch({ type: LOAD_PRODUCTS, payload: products })
   }, [products])
   return (
-    <FilterContext.Provider value='filter context'>
+    <FilterContext.Provider value={{ ...state }}>
       {children}
     </FilterContext.Provider>
   )
