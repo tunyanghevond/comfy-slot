94d01149c5a56fd37817558f71f7ee897ad77529
Ghevond Tunyan <ghtunyan2018@gmail.com>
2022-08-30 20:24:36 +0200

    cart : cart content cart columns

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -4,17 +4,42 @@ import { formatPrice } from '../utils/helpers'
 import AmountButtons from './AmountButtons'
 import { FaTrash } from 'react-icons/fa'
 import { useCartContext } from '../context/cart_context'
-const CartItem = () => {
-  return <h4>cart item</h4>
+const CartItem = ({ id, image, name, color, price, amount }) => {
+  const { removeItem, toggleAmount } = useCartContext()
+  const increase = () => {}
+  const decrease = () => {}
+
+  return (
+    <Wrapper>
+      <div className='title'>
+        <img src={image} alt={name} />
+        <div>
+          <h5 className='name'>{name}</h5>
+          <p className='color'>
+            color : <span style={{ background: color }}></span>
+          </p>
+          <h5 className='price-small'>{formatPrice(price)}</h5>
+        </div>
+      </div>
+      <h5 className='price'>{formatPrice(price)}</h5>
+      <AmountButtons
+        amount={amount}
+        increase={increase}
+        decrease={decrease}
+      ></AmountButtons>
+      <h5 className='subtotal'> {formatPrice(price * amount)}</h5>
+      <button
+        type='button'
+        className='remove-btn'
+        onClick={() => removeItem(id)}
+      >
+        <FaTrash />
+      </button>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.article`
-  .subtotal {
-    display: none;
-  }
-  .price {
-    display: none;
-  }
   display: grid;
   grid-template-columns: 200px auto auto;
   grid-template-rows: 75px;
@@ -23,8 +48,8 @@ const Wrapper = styled.article`
   margin-bottom: 3rem;
   align-items: center;
   .title {
-    grid-template-rows: 75px;
     display: grid;
+    grid-template-rows: 75px;
     grid-template-columns: 75px 125px;
     align-items: center;
     text-align: left;
@@ -59,6 +84,12 @@ const Wrapper = styled.article`
       margin-left: 0.5rem;
       border-radius: var(--radius);
     }
+  }
+  .subtotal {
+    display: none;
+  }
+  .price {
+    display: none;
   }
   .price-small {
     color: var(--clr-primary-5);
