092719bbd5a223a54ea4ea857e4bac2526d86426
Ghevond Tunyan <ghtunyan2018@gmail.com>
2022-09-07 20:20:33 +0200

    stripe : integrate stripe functions in stripeCheckout component

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.render(
   <Auth0Provider
-    domain='dev-008ynj1h.us.auth0.com'
-    clientId='fGQt1AzRrg95JmhJHOnU8w1LDN9BOUkj'
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     redirectUri={window.location.origin}
     cacheLocation='localstorage'
   >
