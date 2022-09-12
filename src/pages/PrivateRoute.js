d527a1cf51c2d1da434b779d16ecabbfef42ffc2
Ghevond Tunyan <ghtunyan2018@gmail.com>
2022-09-12 17:57:30 +0200

    complete project

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import { Route, Redirect } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 import { useAuth0 } from '@auth0/auth0-react'
+import Nav from '../components/Navbar'
 
-const PrivateRoute = ({ children, ...rest }) => {
+const PrivateRoute = ({ children }) => {
   const { user } = useAuth0()
-
-  return (
-    <Route
-      {...rest}
-      render={() => {
-        return user ? children : <Redirect to='/'></Redirect>
-      }}
-    ></Route>
-  )
+  if (!user) {
+    return <Navigate to='/' />
+  }
+  return children
+  // <Route
+  //   {...rest}
+  //   render={() => {
+  //     return user ? children : <Redirect to='/'></Redirect>
+  //   }}
+  // ></Route>
 }
 export default PrivateRoute
